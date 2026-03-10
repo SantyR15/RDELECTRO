4386467c5e991234b6a20f11e436f1a7c83bd028
SantyR15 <119822378+SantyR15@users.noreply.github.com>
2026-03-10 13:27:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbz-0toaVNhpRLnRRqe-LG86IhFYrTxjma0oWpnb7ghafh4VWiNi0gmdz_EiyhDuNjg3/exec'; // Reemplaza con tu link /exec
+const API_URL = 'https://script.google.com/macros/s/AKfycby2Q1M_4UeTCSCLWfYhoDU2L4k-zFv2QlxIUqFALZOz5-LYwlQjV0xLY6LF07daU9Um/exec'; // Reemplaza con tu link /exec
 const grid = document.getElementById('product-grid');
 const searchBar = document.getElementById('searchBar');
 const buttons = document.querySelectorAll('.filter-btn');
@@ -47,6 +47,7 @@ searchBar.addEventListener('input', (e) => {
 
 
 loadCatalog();
+
 
 
 
